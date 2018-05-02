8ec2bdf3752869e0357ae885199ef73433f9bfab
ZhouyihaiDing <ddyihai@google.com>
2018-05-02 16:02:19 -0700

    remove some tests, only leave cross test

--- tests/qps/run_test.sh
+++ tests/qps/run_test.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # LoggingClient
-php -d extension=grpc.so -d extension=protobuf.so Logging/LoggingClientGRPC.php 20 40
-php -d extension=protobuf.so Logging/LoggingClientREST.php 20 40
 php -d extension=grpc.so -d extension=protobuf.so Logging/LoggingClientCrossTests.php 40 80
 
 
